308b4a1912b2471ce38a48ebc6c8169cb62c516f
Daniel Racz <daniel.racz@irob.uni-obuda.hu>
2021-03-22 15:56:34 +0100

    tests extended to cover some basic path on the game

--- src/internal/cards_against_humanity_rules/models.py
+++ src/internal/cards_against_humanity_rules/models.py
@@ -18,6 +18,10 @@ class GameEventException(Exception):
     pass
 
 
+class InvalidPlayerAction(GameEventException):
+    pass
+
+
 class PlayerHasWon(GameEventException):
     pass
 
@@ -29,6 +33,8 @@ class GameHasEnded(GameEventException):
 class GameIsFull(GameEventException):
     pass
 
+class LogicalError(GameEventException):
+    pass
 
 class CardsAgainstHumanityRoles:
     PLAYER = "PLAYER"
@@ -45,6 +51,9 @@ class Submission(BaseModel):
     black_card: BlackCard
     white_cards: List[WhiteCard]
 
+    def __hash__(self):
+        return hash(str(self.dict()))
+
 
 class CardsAgainstHumanityPlayer(BaseModel):
     username: str
@@ -53,6 +62,9 @@ class CardsAgainstHumanityPlayer(BaseModel):
     current_role: str = CardsAgainstHumanityRoles.PLAYER
 
     submissions: List[Submission] = Field(default_factory=list)
+
+    def __hash__(self):
+        return hash(str(self.dict()))
 
     def elect_as_tzar(self):
         self.current_role = CardsAgainstHumanityRoles.TZAR
@@ -89,6 +101,42 @@ class GameSession(BaseModel):
                 return p
         return None
 
+    def __next_active_black_card(self):
+        if len(self.black_cards) == 0:
+            raise GameHasEnded("Game has ended, no more black cards left.")
+        self.currently_active_card = self.black_cards.pop()
+
+    def select_winner(self, selected_submission: Submission):
+        for player, submission in self.player_submissions.items():
+            if submission == selected_submission:
+                player.reward_points(1)
+                self.__start_new_round()
+                return
+
+        raise InvalidPlayerAction(
+            f"Selected submission does not exist! "
+            f"Selected: {selected_submission}, "
+            f"Player submissions: {self.player_submissions}"
+        )
+
+    def __close_round(self):
+        self.state = GameStates.TZAR_CHOOSING_WINNER
+
+    def __start_new_round(self):
+        self.round_count += 1
+        print(f"Starting a new round #{self.round_count}")
+        self.player_submissions.clear()
+        for p in self.players:
+            card_count: int = len(p.cards_in_hand)
+            if card_count < CARDS_IN_PLAYER_HAND:
+                white_cards_for_player = self.white_cards[
+                    0 : CARDS_IN_PLAYER_HAND - card_count
+                ]
+                print(f"Adding cards to {p.username}=> {white_cards_for_player}")
+                p.cards_in_hand += white_cards_for_player
+                for c in white_cards_for_player:
+                    self.white_cards.remove(c)
+
     def player_join(self, username: str):
         white_cards_for_player = self.white_cards[0:CARDS_IN_PLAYER_HAND]
 
@@ -108,20 +156,44 @@ class GameSession(BaseModel):
         if player_to_remove is not None:
             self.players.remove(player_to_remove)
 
-    def start_game(self):
-        if len(self.players) > 2:
+    def start_game(self) -> bool:
+        if len(self.players) >= 2:
             self.players[0].elect_as_tzar()
+            self.__next_active_black_card()
             self.state = GameStates.PLAYERS_SUBMITTING_CARDS
+            return True
+        else:
+            return False
 
     def player_submit_card(self, username: str, cards: List[WhiteCard]):
+        if self.currently_active_card is None:
+            raise InvalidPlayerAction(
+                "Game has not started, but player tried to submit cards."
+            )
+
         player = self.__lookup_player_by_name(username)
         if player is not None:
-            self.player_submissions[player] = Submission(
+            if len(cards) != self.currently_active_card.pick:
+                raise InvalidPlayerAction(
+                    f"{username} tried to submit {len(cards)}, but it should've been {self.currently_active_card.pick}"
+                )
+            submission = Submission(
                 white_cards=cards, black_card=self.currently_active_card
             )
+            player.submissions.append(submission)
+            self.player_submissions[player] = submission
+            for card in cards:
+                player.cards_in_hand.remove(card)
+
+            if (
+                len(self.player_submissions.keys()) == len(self.players) - 1
+            ):  # -1 is the Tzar
+                self.__close_round()
+
         else:
-            FatalException(
-                f"Tried to lookup player: {username}, but was not found. Current player list : {self.players}"
+            raise LogicalError(
+                f"Tried to lookup player: {username}, but was not found."
+                f"Current player list : {self.players}"
             )
 
     @classmethod
@@ -131,3 +203,6 @@ class GameSession(BaseModel):
             white_cards=get_n_random_white_cards(db, round_count * 15),
             black_cards=get_n_random_black_cards(db, round_count),
         )
+
+    def __hash__(self):
+        return hash(str(self.dict()))
--- tests/unit/internal/cards_against_humanity_rules/test_models.py
+++ tests/unit/internal/cards_against_humanity_rules/test_models.py
@@ -1,6 +1,12 @@
+from typing import List
+
 from src.internal.cards_against_humanity_rules.models import (
     GameSession,
     CARDS_IN_PLAYER_HAND,
+    GameStates,
+    CardsAgainstHumanityRoles,
+    CardsAgainstHumanityPlayer,
+    Submission,
 )
 
 
@@ -30,3 +36,53 @@ def test_game_session_creation_and_user_assignment(
     assert len(sess.players) == 1
     player_names = [p.username for p in sess.players]
     assert "Joe" in player_names
+
+
+def test_game_starts_only_with_enough_players(
+    database_connection, prefill_cards_to_database
+):
+    prefill_cards_to_database()
+    db = database_connection
+    sess = GameSession.new_session("test", 5, db)
+
+    assert not sess.start_game()
+    assert sess.state == GameStates.STARTING
+
+    sess.player_join("Joe")
+    sess.player_join("David")
+    sess.player_join("Peter")
+
+    assert sess.start_game()
+    assert sess.state == GameStates.PLAYERS_SUBMITTING_CARDS
+
+    assert sess.currently_active_card.pick is not None
+
+    tzars = [
+        p for p in sess.players if p.current_role == CardsAgainstHumanityRoles.TZAR
+    ]
+    assert len(tzars) == 1
+    the_tzar = tzars[0]
+
+    everyone_else: List[CardsAgainstHumanityPlayer] = [
+        p for p in sess.players if p.current_role == CardsAgainstHumanityRoles.PLAYER
+    ]
+
+    active_card_picks = sess.currently_active_card.pick
+    for player in everyone_else:
+        assert len(player.cards_in_hand) == CARDS_IN_PLAYER_HAND
+        sess.player_submit_card(
+            player.username, player.cards_in_hand[0:active_card_picks]
+        )
+        assert len(player.cards_in_hand) == CARDS_IN_PLAYER_HAND - active_card_picks
+
+    assert sess.state == GameStates.TZAR_CHOOSING_WINNER
+    the_winner_player: CardsAgainstHumanityPlayer = everyone_else[0]
+    winning_submission = the_winner_player.submissions[-1]
+
+    sess.select_winner(winning_submission)
+
+    for p in everyone_else:
+        if p != the_winner_player:
+            assert p.points == 0
+        else:
+            assert p.points == 1
