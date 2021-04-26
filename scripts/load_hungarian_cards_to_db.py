8d928b3385dd24a2bfe7bab9080e86c31751c50b
dracz3 <daniel.racz@irob.uni-obuda.hu>
2021-04-26 20:28:39 +0200

    formatting and generating new openapi

--- scripts/load_hungarian_cards_to_db.py
+++ scripts/load_hungarian_cards_to_db.py
@@ -5,9 +5,14 @@ from src.utils.bootstrapping import load_hungarian_cards_to_dabase
 
 if __name__ == "__main__":
     database = SessionLocal()
-    add_new_deck(database, DeckMetaData(id_name='hungarian',
-                                        description='Hungarian card collection',
-                                        official=False,
-                                        name='hungarian',
-                                        icon='hungarian'))
+    add_new_deck(
+        database,
+        DeckMetaData(
+            id_name="hungarian",
+            description="Hungarian card collection",
+            official=False,
+            name="hungarian",
+            icon="hungarian",
+        ),
+    )
     load_hungarian_cards_to_dabase(database, "resources/hungarian_cards.json")
--- scripts/load_official_decks_to_db.py
+++ scripts/load_official_decks_to_db.py
@@ -1,37 +1,46 @@
 import json
 
-from src.cards.crud import add_multiple_new_white_card, add_multiple_new_black_card, add_new_deck
+from src.cards.crud import (
+    add_multiple_new_white_card,
+    add_multiple_new_black_card,
+    add_new_deck,
+)
 from src.cards.models import WhiteCard, BlackCard, DeckMetaData
 from src.database.database import SessionLocal
 
 
 if __name__ == "__main__":
     database = SessionLocal()
-    with open("resources/official-decks.json", 'r') as f:
+    with open("resources/official-decks.json", "r") as f:
         data = json.load(f)
 
     for pack in data:
-        deck_name = pack['name']
+        deck_name = pack["name"]
         print(f"Processing {deck_name}")
 
-        add_new_deck(database,DeckMetaData(id_name = deck_name,
-                     description = deck_name,
-                     official = pack['official'],
-                     name = deck_name,
-                     icon = deck_name) )
+        add_new_deck(
+            database,
+            DeckMetaData(
+                id_name=deck_name,
+                description=deck_name,
+                official=pack["official"],
+                name=deck_name,
+                icon=deck_name,
+            ),
+        )
 
         white_cards = []
         black_cards = []
-        for w in pack['white']:
+        for w in pack["white"]:
             white_cards.append(
-                WhiteCard(text=w['text'], icon=deck_name, deck=deck_name)
+                WhiteCard(text=w["text"], icon=deck_name, deck=deck_name)
             )
         add_multiple_new_white_card(database, white_cards)
 
-        for b in pack['black']:
+        for b in pack["black"]:
             black_cards.append(
-                BlackCard(text = b['text'],
-                          icon=deck_name, deck=deck_name,
-                          pick = b['pick'])
-             )
+                BlackCard(
+                    text=b["text"], icon=deck_name, deck=deck_name, pick=b["pick"]
+                )
+            )
         add_multiple_new_black_card(database, black_cards)
