a681400595d7f4bcb03d354176977642c502e0f4
dRacz3 <daniel.racz@irob.uni-obuda.hu>
2021-05-17 18:27:52 +0200

    fix error message

--- src/routers/cards.py
+++ src/routers/cards.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import src.cards.crud
@@ -18,20 +18,29 @@ router = APIRouter(
 # TODO: add tests for source checking over endpoint
 @router.put("/white", response_model=src.cards.models.WhiteCard)
 def create_white_card(card: src.cards.models.WhiteCard, db: Session = Depends(get_db)):
-    _ = src.cards.crud.add_new_white_card(db, card, source="user")
-    return card
+    try:
+        _ = src.cards.crud.add_new_white_card(db, card, source="user")
+        return card
+    except Exception as e:
+        raise HTTPException(400, f"Failed to add card. {e}")
 
 
 @router.put("/black", response_model=src.cards.models.BlackCard)
 def create_black_card(card: src.cards.models.BlackCard, db: Session = Depends(get_db)):
-    _ = src.cards.crud.add_new_black_card(db, card, source="user")
-    return card
+    try:
+        _ = src.cards.crud.add_new_black_card(db, card, source="user")
+        return card
+    except Exception as e:
+        raise HTTPException(400, f"Failed to add card. {e}")
 
 
 @router.put("/deck", response_model=src.cards.models.DeckMetaData)
 def create_deck(deck: src.cards.models.DeckMetaData, db: Session = Depends(get_db)):
-    _ = src.cards.crud.add_new_deck(db, deck, source="user")
-    return deck
+    try:
+        _ = src.cards.crud.add_new_deck(db, deck, source="user")
+        return deck
+    except Exception as e:
+        raise HTTPException(400, f"Failed to add deck. {e}")
 
 
 # TODO : add test for it
