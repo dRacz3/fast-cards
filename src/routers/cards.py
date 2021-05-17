f001bf027b407218560e2561afffb11d4d52da41
dRacz3 <daniel.racz@irob.uni-obuda.hu>
2021-05-17 14:52:12 +0200

    submitting card/deck over endpoints will add source = 'user'

--- src/routers/cards.py
+++ src/routers/cards.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import Session
 import src.cards.crud
 import src.cards.models
 from src.auth.auth_bearer import JWTBearer
-from src.auth.auth_handler import decodeJWT
 
 from src.dependencies import get_db
 
@@ -16,17 +15,23 @@ router = APIRouter(
     tags=["cards"],
 )
 
-
+# TODO: add tests for source checking over endpoint
 @router.put("/white", response_model=src.cards.models.WhiteCard)
 def create_white_card(card: src.cards.models.WhiteCard, db: Session = Depends(get_db)):
-    _ = src.cards.crud.add_new_white_card(db, card)
+    _ = src.cards.crud.add_new_white_card(db, card, source="user")
     return card
 
 
 @router.put("/black", response_model=src.cards.models.BlackCard)
 def create_black_card(card: src.cards.models.BlackCard, db: Session = Depends(get_db)):
-    _ = src.cards.crud.add_new_black_card(db, card)
+    _ = src.cards.crud.add_new_black_card(db, card, source="user")
     return card
+
+
+@router.put("/deck", response_model=src.cards.models.DeckMetaData)
+def create_deck(deck: src.cards.models.DeckMetaData, db: Session = Depends(get_db)):
+    _ = src.cards.crud.add_new_deck(db, deck, source="user")
+    return deck
 
 
 # TODO : add test for it
